51f2e75c6f673e32617d51952283ae49e81fa1a7
Blagovest Petrov <blagovest@petrovs.info>
2016-05-04 15:09:03 +0300

    Manifests cleaned according to the Puppet code styling standarts

--- manifests/refinery.pp
+++ manifests/refinery.pp
@@ -4,30 +4,28 @@ include stdlib
 Class['apt::update'] -> Package <| provider == 'apt' |>
 
 $packages = [
-	"ruby2.0",
-	"ruby2.0-dev",
-	"ruby-execjs",
-	"sqlite3",
-	"libmysqlclient-dev",
-	"libsqlite3-dev",
-	"libpq-dev",
-	"imagemagick",
-	"libmagickwand-dev",
-	"libmagickcore-dev",
-	"build-essential",
-	"git",
-	"g++",
-	"libxml2",
-	"libxml2-dev",
-	"libxslt-dev",
-	"libcurl4-openssl-dev",
-]  
-
+  'ruby2.0',
+  'ruby2.0-dev',
+  'ruby-execjs',
+  'sqlite3',
+  'libmysqlclient-dev',
+  'libsqlite3-dev',
+  'libpq-dev',
+  'imagemagick',
+  'libmagickwand-dev',
+  'libmagickcore-dev',
+  'build-essential',
+  'git',
+  'g++',
+  'libxml2',
+  'libxml2-dev',
+  'libxslt-dev',
+  'libcurl4-openssl-dev',
+]
 
 apt::ppa { 'ppa:brightbox/ruby-ng':
 } ->
 
-
-package  { $packages:
-    ensure   => installed,
+package { $packages:
+  ensure   => installed,
 }
